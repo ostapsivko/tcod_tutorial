1a2b98c2466904b62acf0445977943a39c55989d
ostapsivko <o-stap@hotmail.com>
2025-01-09 01:09:10 +0200

    Part 4 - FOV handling

--- engine.py
+++ engine.py
@@ -2,6 +2,7 @@ from typing import Set, Iterable, Any
 
 from tcod.context import Context
 from tcod.console import Console
+from tcod.map import compute_fov
 
 from actions import EscapeAction, MovementAction
 from entity import Entity
@@ -14,6 +15,7 @@ class Engine:
         self.event_handler = event_handler
         self.player = player
         self.map = map
+        self.update_fov()
 
     def handle_events(self, events:Iterable[Any]) -> None:
         for event in events:
@@ -24,12 +26,25 @@ class Engine:
 
             action.perform(self, self.player)
 
+            self.update_fov()
+
     def render(self, console:Console, context:Context) -> None:
         self.map.render(console)
         
         for entity in self.entities:
-            console.print(entity.x, entity.y, entity.char, fg=entity.color)
+            if self.map.visible[entity.x, entity.y]:
+                console.print(entity.x, entity.y, entity.char, fg=entity.color)
 
         context.present(console)
 
         console.clear()
+
+    def update_fov(self) -> None:
+        """Recompute the visible area based on the player current point of view"""
+        self.map.visible[:] = compute_fov(
+            self.map.tiles["transparent"],
+            (self.player.x, self.player.y),
+            radius=8
+        )
+
+        self.map.explored |= self.map.visible
--- game_map.py
+++ game_map.py
@@ -8,9 +8,24 @@ class GameMap:
         self.width, self.height = width, height
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
 
+        self.visible = np.full((width, height), fill_value=False, order="F")
+        self.explored = np.full((width, height), fill_value=False, order="F")
+
     def in_bounds(self, x:int, y:int) -> bool:
         """Return True if x and y are in bounds of the map"""
         return 0 <= x < self.width and 0 <= y < self.height
     
     def render(self, console:Console) -> None:
-        console.tiles_rgb[0:self.width, 0:self.height] = self.tiles["dark"]
+        """Renders the map
+        
+        If a tile is in the "visible" array, then draw it with the "light" colors.
+        If it is not visible, but explored, use dark color.
+        
+        Otherwise it's drawn as shroud.
+        """
+
+        console.tiles_rgb[0:self.width, 0:self.height] = np.select(
+            condlist=[self.visible, self.explored],
+            choicelist=[self.tiles["light"], self.tiles["dark"]],
+            default=tile_types.SHROUD
+        )
